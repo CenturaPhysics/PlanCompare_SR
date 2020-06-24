7a4a9c3432020d6ef6cb90ee8abfccf0c220d8cb
antoneagle <eagleanton@yahoo.com>
2020-06-24 16:43:09 -0600

    Add project files.

---
+++ PlanCompare_SR.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using VMS.TPS.Common.Model.API;
+using VMS.TPS.Common.Model.Types;
+using PlanCompare_SR;
+using PlanCompare_SR.Views;
+
+// TODO: Replace the following version attributes by creating AssemblyInfo.cs. You can do this in the properties of the Visual Studio project.
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
+[assembly: AssemblyInformationalVersion("1.0")]
+
+// TODO: Uncomment the following line if the script requires write access.
+// [assembly: ESAPIScript(IsWriteable = true)]
+
+namespace VMS.TPS {
+    public class Script {
+        public Script()
+        {
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+
+        public void Execute(ScriptContext context, Window mainWindow)
+        {
+            // TODO : Add here the code that is called when the script is launched from Eclipse.
+            Run(context.CurrentUser, context.Patient, context.Image, context.StructureSet, context.PlanSetup, context.PlansInScope, context.PlanSumsInScope, mainWindow);
+
+
+            //mainWindow wnd = new mainWindow();
+
+            //wnd.DataContext = context;
+            //wnd.theContext = context;
+
+            //wnd.ShowDialog();
+            //Test
+
+        }
+
+        public void Run( User user, Patient patient, Image image, StructureSet structureSet, PlanSetup planSetup,
+        IEnumerable<PlanSetup> planSetupsInScope, IEnumerable<PlanSum> planSumsInScope, Window mainWindow)
+        {
+            // Your main code now goes here
+
+            //Use this version for PlugIn version
+            //mainWindowContents wndContents = new mainWindowContents();
+
+            //Use this for ScriptRunner version
+            mainWindowContents wndContents = new mainWindowContents(patient, planSetup);
+
+            mainWindow.Width = wndContents.Width;
+            mainWindow.Height = wndContents.Height;
+            mainWindow.Content = wndContents;
+
+            //Show the main window.
+            mainWindow.ShowDialog();
+            
+        }
+
+    }
+}
---
+++ Runner/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using VMS.TPS;
+using VMS.TPS.Common.Model.API;
+using VMS.TPS.Common.Model.Types;
+using PlanCompare_SR;
+using EclipsePlugInRunner.Scripting;
+
+namespace PlanCompare_SR.Runner {
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    
+    public partial class App : System.Windows.Application {
+        private void App_OnStartup(object sender, StartupEventArgs e)
+        {
+            ScriptRunner.Run(new Script());
+        }
+
+        // Fix UnauthorizedScriptingAPIAccessException
+        public void DoNothing(PlanSetup plan) { }
+    }
+}
